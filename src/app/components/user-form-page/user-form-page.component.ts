a340b41ffff28db25e52a8d1f17e33ce0e049f9f
Rishi Goyal <rishig516@gmail.com>
2024-12-27 22:53:53 +0530

    angular setup

---
+++ src/app/components/user-form-page/user-form-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-form-page.component.html',
+  styleUrl: './user-form-page.component.css'
+})
+export class UserFormPageComponent {
+
+}
